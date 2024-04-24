a4802758894618cdb9052148b35b3658e4469f72
Wesley Oliveira Silva <will.lg@hotmail.com>
2024-04-24 20:35:29 -0300

    skills done

--- portifolio/src/components/section/Skills.js
+++ portifolio/src/components/section/Skills.js
@@ -1,7 +1,22 @@
+import styles from './Skills.module.css'
+import javascript from '../../images/skills/javascript.svg'
+import css from '../../images/skills/css.svg'
+import html from '../../images/skills/html.svg'
+import typescript from '../../images/skills/typescript.svg'
+import react from '../../images/skills/react.svg'
+
 function Skills(){
     return(
-        <div id='Skills'>
-            <h1>Aqui ficará minhas a</h1>
+        <div className={styles.skill} id='Skills'>
+            <h1>Habilidades</h1>
+            <p>Conheça um pouco das minahs habildiades e conhecimentos</p>
+            <div>
+                <img src={html} alt='html logo'></img>
+                <img src={css} alt='css logo'></img>
+                <img src={javascript} alt='javacript logo'></img>
+                <img src={react} alt='react logo'></img>
+                <img src={typescript} alt='typescript logo'></img>
+            </div>
         </div>
     )
 }
